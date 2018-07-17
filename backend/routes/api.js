1aa57bb33fed762321ebc1b773ed2652150e4819
nazarii.viazivskyi <nazarii.viazivskyi@globallogic.com>
2018-07-17 14:31:36 +0300

    Refactored api.js and auth.js files

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -17,5 +17,73 @@ router.get('/users', (req, res) => {
 
 });
 
+router.put('/request-role/user/:id', (req, res) => {
+
+    User.findById(req.params.id, (err, user) => {
+        if (err) {
+            return res.status(500).send({
+                success: false,
+                message: 'Server error.'
+            });
+        }
+
+        user.request = {
+            reason: req.body.reason,
+            requestDate: new Date()
+        };
+
+        user.roles.__global_roles__ = ['wait-role'];
+
+        user.save((err, user) => {
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'Server error.'
+                });
+            }
+            res.json(user);
+        });
+
+    });
+});
+
+router.put('/set-role/user/:id', (req, res) => {
+
+    User.findById(req.params.id, (err, user) => {
+        if (err) {
+            return res.status(500).send({
+                success: false,
+                message: 'Server error.'
+            });
+        }
+
+        user.roles.__global_roles__.splice(0, 1, req.body.role);
+
+        user.save((err, user) => {
+            if (err) {
+                return res.status(500).send({
+                    success: false,
+                    message: 'Server error.'
+                });
+            }
+            res.json(user);
+        });
+
+    });
+});
+
+router.delete('/delete-user/:id', (req, res) => {
+
+    User.remove({_id: req.params.id}, function (err, data) {
+        if (err) {
+            return res.status(500).send({
+                success: false,
+                message: 'Server error.'
+            });
+        }
+        res.json(data);
+    });
+
+});
 
 module.exports = router;
